5f62fcfc20b166785257c9a439d26f7ab9c02c93
OliverMeredith <mail@olivermeredith.com>
2019-04-23 20:01:20 +0100

    Add logo

--- src/index.js
+++ src/index.js
@@ -16,53 +16,52 @@ class AppRouter extends Component{
   }
 
   showAbout(){
-    console.log("click")
     this.setState({showAbout: !this.state.showAbout})
   }
+
+  
   render(){
     return (
       <>
-          <nav>
-            <ul>
+        <nav>
+          <ul>
+            <li onClick={this.showAbout.bind(this)}>
+              <img src={this.state.showAbout ? 'assets/home-icon.png' : 'assets/info-icon.png'} alt=""/>
+            </li>
+          </ul>
+        </nav>
+    
+        <main className="container">
+          <Transition
+            native
+            items = {!this.state.showAbout}
+            from = {{ opacity: 0, transform: 'translateY(50px)'}}
+            enter = {{ opacity: 1, transform: 'translateY(0)'}}
+            leave = {{ opacity: 0, transform: 'translateY(50px)'}}
+          >
+            {show => show && (props => (
+              <animated.div style={props}>
+                <VideoScreen/>
+              </animated.div>
+            ))}
+          
+          </Transition>
 
-              <li onClick={this.showAbout.bind(this)}>
-                <img src={this.state.showAbout ? 'assets/home-icon.png' : 'assets/info-icon.png'} alt=""/>
-              </li>
-
-            </ul>
-          </nav>
-      
-      <main className="container">
-      <Transition
-        native
-        items = {!this.state.showAbout}
-        from = {{ opacity: 0, transform: 'translateY(50px)'}}
-        enter = {{ opacity: 1, transform: 'translateY(0)'}}
-        leave = {{ opacity: 0, transform: 'translateY(50px)'}}
-      >
-      {show => show && (props => (
-        <animated.div style={props}>
-          <VideoScreen/>
-        </animated.div>
-      ))}
-      
-      </Transition>
-
-        <Transition
-          native
-          items = {this.state.showAbout}
-          from = {{ opacity: 0}}
-          enter = {{ opacity: 1}}
-          leave = {{ opacity: 0}}
-          delay = {this.state.showAbout ? 300 : 0}
-        >
-        {show => show && (props => (
-          <animated.div style={props}>
-              <About/>
-          </animated.div>
-        ))}
-        </Transition>
-      </main>
+          <Transition
+            native
+            items = {this.state.showAbout}
+            from = {{ opacity: 0}}
+            enter = {{ opacity: 1}}
+            leave = {{ opacity: 0}}
+            delay = {this.state.showAbout ? 300 : 0}
+          >
+            {show => show && (props => (
+              <animated.div style={props}>
+                  <About/>
+              </animated.div>
+            ))}
+          </Transition>
+        </main>
 
       </>
     
